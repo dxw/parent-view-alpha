0728bf43cd3a8ecea8a36d7fb53b15add4345079
Gaz Aston <gaz@dxw.com>
2019-03-12 16:46:51 +0000

    (feature) add dynamic URLs for questions

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
+var questions = require('./data/questions.json')
+
 // Add your routes here - above the module.exports line
+
+router.get('/leave-feedback/questions/:questionId', function(req, res) {
+  var question = req.params.questionId
+  res.render('leave-feedback/questions/question', {
+    'question': question,
+    'text': questions.data.find(obj => String(obj.id) === question).text,
+    'hintText': questions.data.find(obj => String(obj.id) === question).hintText,
+    'nextQuestion': Number(question) + 1
+  });
+});
 
 module.exports = router
