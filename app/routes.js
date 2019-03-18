63ed0973d4f11268eada04cd931337209e6fff8b
Gaz Aston <gaz@dxw.com>
2019-03-18 09:00:15 +0000

    (chore) adds lots of code comments
    I’ve added these for two reasons:
    - it’ll help make things clearer for refactoring
    - it’ll be easier for someone else to see what I’ve done

--- app/routes.js
+++ app/routes.js
@@ -1,23 +1,32 @@
 const express = require('express')
 const router = express.Router()
 
+// imports the contents of questions.json, sets to a variable
 var questions = require('./data/questions.json')
 
 function sanitiseText(text) {
+  // replaces spaces with hyphens and removes commas and full stops
   return text.replace(/\s+/g, '-').replace(/[.’]/g, '').toLowerCase();
 }
 
 // Add your routes here - above the module.exports line
 
 router.get('/leave-feedback/questions/:questionId', function(req, res) {
+  // get the question ID from the URL params hash
   var question = req.params.questionId
+  // find the text of the question in the 'questions' array whose ID matches the one in the params
   var text = questions.data.find(obj => String(obj.id) === question).text
+  // find the hint text of the question in the 'questions' array whose ID matches the one in the params
   var hintText = questions.data.find(obj => String(obj.id) === question).hintText
+  // the next question ID is the current one, plus 1
   var nextQuestion = Number(question) + 1
+  // the last question ID is the current one, minus 1
   var lastQuestion = Number(question) - 1
+  // the question's text, sanitised
   var sanitisedQuestionText = sanitiseText(text)
+  // gets the number of questions in questions.json
   var questionCount = questions.data.length
-  var currentQuestion = Number(question)
+  // checks for the 'edit' parameter in the URL query string
   var editing = req.query.edit
   res.render('leave-feedback/questions/question', {
     'question': question,
@@ -27,7 +36,6 @@ router.get('/leave-feedback/questions/:questionId', function(req, res) {
     'lastQuestion': lastQuestion,
     'sanitisedQuestionText': sanitisedQuestionText,
     'questionCount': questionCount,
-    'currentQuestion': currentQuestion,
     'editing': editing
   });
 });
